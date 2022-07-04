e977a8befeb8fc707a13005c0d6a421d7db418a5
alihaidermalik20 <95946965+alihaidermalik20@users.noreply.github.com>
2022-07-04 14:14:40 +0200

    function and tests

--- doctor-example.test.js
+++ doctor-example.test.js
@@ -41,22 +41,145 @@
 
 // =============== your solutions will go here ===============
 
+// const alex = {
+//     profession: 'doctor',
+//     experience: 20,
+// };
+// bonus(alex, 200);
 
+function bonus(employee, premium) {
+  let payout = 0;
+  if (employee.profession === 'doctor') {
+    if (employee.experience >= 10 && employee.experience < 20) {
+      payout = premium * 2;
+    } else if (employee.experience >= 20) {
+      payout = premium * 3;
+    } else if (employee.experience < 10) {
+      payout = premium * 1.5;
+    }
+    if (payout > 1000) {
+      throw new Error('doctor bonus greater than 1000');
+    }
+  } else if (employee.profession === 'nurse') {
+    if (employee.experience < 10) {
+      payout = premium * 1.2;
+    } else if (employee.experience >= 10 && employee.experience < 15) {
+      payout = premium * 1.3;
+    } else if (employee.experience >= 15 && employee.experience < 20) {
+      payout = premium * 1.5;
+    } else if (employee.experience >= 20) {
+      payout = premium * 1.7;
+    }
+    if (payout > 500) {
+      throw new Error('nurse bonus greater than 500');
+    }
+  }
+  payout = Math.floor(payout);
+  return payout;
+}
+
+console.log(bonus({ profession: 'nurse', experience: 25 }, 400));
 // =============== a for-of loop to control which solution(s) are tested ===============
 
-for (const solution of [calculatePremium]) {
+for (const solution of [bonus]) {
   // =============== test cases for this challenge ===============
 
-  describe(solution.name + ': _', () => {
-    describe('When the premium is 200', () => {
-        it('Should multiply by x1,2 when she has less than 10 year experience',
-            () => {
-                expect((solution({profession: 'nurse', experience: 8}, 200))).toBe(240)
-        });
-    })
+  describe(`${solution.name}: returns the bonus amount`, () => {
+    describe('gives correct bonus to all the doctors', () => {
+      describe('gives correct bonus to doctors with less than 10 years of experience', () => {
+        for (let i = 0; i < 10; i++) {
+          it('return 200 * 1.5 = 300 for less than 10y exp', () => {
+            expect(solution({ profession: 'doctor', experience: i }, 200)).toBe(300);
+          });
+          it('return 400 * 1.5 = 600 for less than 10y exp', () => {
+            expect(solution({ profession: 'doctor', experience: i }, 400)).toBe(600);
+          });
+          it('return 600 * 1.5 = 900 for less than 10y exp', () => {
+            expect(solution({ profession: 'doctor', experience: i }, 600)).toBe(900);
+          });
+        }
+      });
+      describe('gives correct bonus to doctors 10 to 19 years of exp', () => {
+        for (let i = 10; i < 20; i++) {
+          it('200 * 2 = 400', () => {
+            expect(solution({ profession: 'doctor', experience: i}, 200)).toBe(400);
+          });
+          it('400 * 2 = 800', () => {
+            expect(solution({ profession: 'doctor', experience: i}, 400)).toBe(800);
+          });
+          it('600 * 2 = ERROR', () => {
+            expect(() => solution({ profession: 'doctor', experience: i}, 600)).toThrowError(new Error('doctor bonus greater than 1000'));
+          });
+        }
+      })
+      describe('gives correct bonus to doctors with 20 and more years of experience upto 55y of exp', () => {
+        for (let i = 20; i <= 55; i++) {
+          it('200 * 3 = 600', () => {
+            expect(solution({ profession: 'doctor', experience: i}, 200)).toBe(600);
+          });
+          it('400 * 3 = ERROR', () => {
+            expect(() => solution({ profession: 'doctor', experience: i}, 400)).toThrowError(new Error('doctor bonus greater than 1000'));
+          });
+          it('600 * 3 = ERROR', () => {
+            expect(() => solution({ profession: 'doctor', experience: i}, 600)).toThrowError(new Error('doctor bonus greater than 1000'));
+          });
+        }
+      })
+    });
+    describe('gives correct bonus to all the nurses', () => {
+      describe('gives correct bonus to nurses with less than 10 years of experience', () => {
+        for (let i = 0; i < 10; i++) {
+          it('return 200 * 1.2 = 240 for less than 10y exp', () => {
+            expect(solution({ profession: 'nurse', experience: i }, 200)).toBe(240);
+          });
+          it('return 400 * 1.2 = 480 for less than 10y exp', () => {
+            expect(solution({ profession: 'nurse', experience: i }, 400)).toBe(480);
+          });
+          it('return 600 * 1.2 = ERROR for less than 10y exp', () => {
+            expect(solution({ profession: 'nurse', experience: i }, 600)).toThrowError(new Error('nurse bonus greater than 500'));
+          });
+        }
+      });
+      describe('gives correct bonus to nurses 10 to 14 years of exp', () => {
+        for (let i = 10; i < 15; i++) {
+          it('200 * 1.3 = 260', () => {
+            expect(solution({ profession: 'nurse', experience: i}, 200)).toBe(260);
+          });
+          it('400 * 1.3 = ERROR', () => {
+            expect(solution({ profession: 'nurse', experience: i}, 400)).toThrowError(new Error('nurse bonus greater than 500'));
+          });
+          it('600 * 1.3 = ERROR', () => {
+            expect(() => solution({ profession: 'nurse', experience: i}, 600)).toThrowError(new Error('nurse bonus greater than 500'));
+          });
+        }
+      })
+      describe('gives correct bonus to nurses with 15 to 19 years of exp', () => {
+        for (let i = 15; i < 20; i++) {
+          it('200 * 1.5 = 300', () => {
+            expect(solution({ profession: 'nurse', experience: i}, 200)).toBe(300);
+          });
+          it('400 * 1.5 = ERROR', () => {
+            expect(solution({ profession: 'nurse', experience: i}, 400)).toThrowError(new Error('nurse bonus greater than 500'));
+          });
+          it('600 * 1.5 = ERROR', () => {
+            expect(() => solution({ profession: 'nurse', experience: i}, 600)).toThrowError(new Error('nurse bonus greater than 500'));
+          });
+        }
+      })
+      describe('gives correct bonus to nurses with 20 or more years of exp upto 55y', () => {
+        for (let i = 20; i <= 55; i++) {
+          it('200 * 1.7 = 340', () => {
+            expect(solution({ profession: 'nurse', experience: i}, 200)).toBe(340);
+          });
+          it('400 * 1.7 = ERROR', () => {
+            expect(solution({ profession: 'nurse', experience: i}, 400)).toThrowError(new Error('nurse bonus greater than 500'));
+          });
+          it('600 * 1.7 = ERROR', () => {
+            expect(() => solution({ profession: 'nurse', experience: i}, 600)).toThrowError(new Error('nurse bonus greater than 500'));
+          });
+        }
+      })
+    });
   });
 }
 
-// =============== a minified solution you can use to test your test cases ===============
-
-// prettier-ignore
